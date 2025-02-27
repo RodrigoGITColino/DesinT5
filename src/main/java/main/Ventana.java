327abb4fb9ec46ed0259eafd3d970d5b674b6053
Rodrigo <rodrigocid2002@gmail.com>
2025-02-27 12:49:16 +0100

    fewfwe

--- src/main/java/main/Ventana.java
+++ src/main/java/main/Ventana.java
@@ -1,8 +1,8 @@
+package main;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
 
 public class Ventana {
 
@@ -38,10 +38,6 @@ public class Ventana {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
-		
-		JLabel lblNewLabel = new JLabel("Diego ayuda");
-		frame.getContentPane().add(lblNewLabel);
 	}
 
 }
